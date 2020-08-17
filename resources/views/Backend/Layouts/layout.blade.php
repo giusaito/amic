d3fa6392fa994fa7b53ab26e131f7be217682462
Giuliano Saito <giulianosaito@iMac-de-Giuliano.local>
2020-08-17 17:38:59 -0300

    fazendo o layout

---
+++ resources/views/Backend/Layouts/layout.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html class="no-js css-menubar" lang="pt-BR">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+  <meta name="description" content="BEWWEB Admin">
+  <meta name="author" content="BEWWEB">
+
+  <title>Área Administrativa</title>
+
+  <link rel="apple-touch-icon" href="{{ URL::asset('public/favicon/apple-icon-180x180.png') }}">
+  <link rel="shortcut icon" href="{{ URL::asset('public/favicon.png') }}">
+
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="{{ URL::asset('public/css/global/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ URL::asset('public/css/global/bootstrap-extend.min.css') }}">
+  <link rel="stylesheet" href="assets/css/site.min.css">
+
+  <!-- Skin tools (demo site only) -->
+  <link rel="stylesheet" href="../global/css/skintools.min.css">
+  <script src="assets/js/Plugin/skintools.min.js"></script>
+
+  <!-- Plugins -->
+  <link rel="stylesheet" href="../global/vendor/animsition/animsition.min.css">
+  <link rel="stylesheet" href="../global/vendor/asscrollable/asScrollable.min.css">
+  <link rel="stylesheet" href="../global/vendor/switchery/switchery.min.css">
+  <link rel="stylesheet" href="../global/vendor/intro-js/introjs.min.css">
+  <link rel="stylesheet" href="../global/vendor/slidepanel/slidePanel.min.css">
+  <link rel="stylesheet" href="../global/vendor/flag-icon-css/flag-icon.min.css">
+  <link rel="stylesheet" href="../global/vendor/waves/waves.min.css">
+
+  <!-- Plugins For This Page -->
+  <link rel="stylesheet" href="../global/vendor/chartist/chartist.min.css">
+  <link rel="stylesheet" href="../global/vendor/jvectormap/jquery-jvectormap.min.css">
+  <link rel="stylesheet" href="../global/vendor/chartist-plugin-tooltip/chartist-plugin-tooltip.min.css">
+
+  <!-- Page -->
+  <link rel="stylesheet" href="assets/examples/css/dashboard/v1.min.css">
+
+  <!-- Fonts -->
+  <link rel="stylesheet" href="../global/fonts/material-design/material-design.min.css">
+  <link rel="stylesheet" href="../global/fonts/brand-icons/brand-icons.min.css">
+  <link rel='stylesheet' href="https://fonts.googleapis.com/css?family=Roboto:400,400italic,700">
+
+
+  <!--[if lt IE 9]>
+    <script src="../global/vendor/html5shiv/html5shiv.min.js"></script>
+    <![endif]-->
+
+  <!--[if lt IE 10]>
+    <script src="../global/vendor/media-match/media.match.min.js"></script>
+    <script src="../global/vendor/respond/respond.min.js"></script>
+    <![endif]-->
+
+  <!-- Scripts -->
+  <script src="../global/vendor/breakpoints/breakpoints.min.js"></script>
+  <script>
+    Breakpoints();
+  </script>
+</head>
+<body class="animsition dashboard">
+</body>
+</html>
