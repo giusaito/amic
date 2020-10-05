51c6e7706a12a1f8d5919f42ca15f61210342b49
Giuliano Saito <giulianosaito@iMac-de-Giuliano.local>
2020-10-05 15:16:20 -0300

    baixando

--- resources/views/Backend/Projetos/index.blade.php
+++ resources/views/Backend/Projetos/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <section id="link-util">
-    <projetos-component home-route="{{ route('backend.index', [], false) }}" list-route="{{ route('backend.projects.lista', [], false) }}"></projetos-component>
+    <projetos-component home-route="{{ route('backend.index', [], false) }}" list-route="{{ route('backend.projetos.lista', [], false) }}"></projetos-component>
 </section>
 
 @endsection
