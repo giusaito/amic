8ecfaa60b1677dcf8b31b235460d95745b10ac20
Leonardo Nascimento <leonardo.nascimento21@gmail.com>
2020-12-02 17:25:09 -0300

    noticia frontend

---
+++ resources/views/Frontend/Article/Themes/Includes/newsletter.blade.php
@@ -0,0 +1,16 @@
+<div class="newsletter mt-5">
+    <h1 class="offset-1 pt-5"><i class="fas fa-envelope-open-text"></i> Inscrava-se na nossa <strong>Newsletter</strong></h1>
+    <form class="offset-1 pt-4">
+        <div class="row">
+            <div class="col">
+                <input type="text" class="form-control" id="name" placeholder="Nome" name="name">
+            </div>
+            <div class="col">
+                <input type="email" class="form-control" placeholder="E-mail" name="email">
+            </div>
+            <div class="col">
+                <button class="btn btn-amic" type="submit">INCREVER-SE</button>
+            </div>
+        </div>
+    </form>
+</div>
--- resources/views/Frontend/Article/Themes/modelo-1/index.blade.php
+++ resources/views/Frontend/Article/Themes/modelo-1/index.blade.php
@@ -6,10 +6,10 @@
   		<div class="container">
 		    <div class="row">
 				<div class="col-12">
-					<hgroup>
-						<h1 class="font-weight-bold">{{$article->title}}</h1>
-						<h5>{{$article->description}}</h5>
-						<h6 class="info-published">Publicado em  <i class="far fa-calendar-alt"></i> 04/11/1992 às <i class="far fa-clock"></i> 08:32 por <i class="fas fa-user-circle"></i> AMIC | ECONOMIA</h6>
+					<hgroup class="article-title">
+						<h1>{{$article->title}}</h1>
+						<h4>{{$article->description}}</h4>
+						<h6 class="info-published">Publicado em <i class="far fa-calendar-alt"></i> {{date('d/m/Y', strtotime($article->published_at))}} às <i class="far fa-clock"></i> {{date('H:m', strtotime($article->published_at))}} por <i class="fas fa-user-circle"></i> {{$article->user->name}} | ECONOMIA</h6>
 					</hgroup>
 					@if($article->video)
 						<div class="embed-responsive embed-responsive-21by9">
@@ -37,8 +37,11 @@
 						{!! $article->content !!}
 					</div>
 					@if($article->photos)
-						@include('Frontend.Article.Themes.Includes.photos')
+						<div class="pt-2 pb-3">
+							@include('Frontend.Article.Themes.Includes.photos')
+						</div>
 					@endif
+						@include('Frontend.Article.Themes.Includes.newsletter')
   				</div>
 				<div class="col-3 mt-5">
 					<p class="text-center">Publicidade</p>
@@ -65,5 +68,73 @@
 
 .share a i {
 	margin-top:3px;
+}
+.article-title h1 {
+	font-family: louis_george_cafebold, sans-serif;
+}
+
+.article-title h4, .article-title h6{
+	margin-top:12px;
+}
+
+.newsletter {
+	background-image: url(https://i.imgur.com/FB4KXfB.jpg);
+	width: 100%;
+	height: 250px;
+	background-size:cover;
+}
+
+.newsletter h1 {
+	color:#fff !important;
+}
+
+.newsletter i {
+	color: #f27a28;
+}
+
+.btn-amic { 
+  color: #FFFFFF !important; 
+  background-color: #FB6629 !important;  
+  border-color: #FB6629 !important; 
+} 
+ 
+.btn-amic:hover, 
+.btn-amic:focus, 
+.btn-amic:active, 
+.btn-amic.active, 
+.open .dropdown-toggle.btn-amic { 
+  color: #FFFFFF !important; 
+  background-color: #000000 !important; 
+  border-color: #000000 !important;
+} 
+ 
+.btn-amic:active, 
+.btn-amic.active, 
+.open .dropdown-toggle.btn-amic { 
+  background-image: none; 
+} 
+ 
+.btn-amic.disabled, 
+.btn-amic[disabled], 
+fieldset[disabled] .btn-amic, 
+.btn-amic.disabled:hover, 
+.btn-amic[disabled]:hover, 
+fieldset[disabled] .btn-amic:hover, 
+.btn-amic.disabled:focus, 
+.btn-amic[disabled]:focus, 
+fieldset[disabled] .btn-amic:focus, 
+.btn-amic.disabled:active, 
+.btn-amic[disabled]:active, 
+fieldset[disabled] .btn-amic:active, 
+.btn-amic.disabled.active, 
+.btn-amic[disabled].active, 
+fieldset[disabled] .btn-amic.active { 
+  background-color: #FB6629; 
+  border-color: #FB6629; 
+} 
+ 
+.btn-amic .badge { 
+  color: #FB6629; 
+  background-color: #FFFFFF; 
 }
 </style>
